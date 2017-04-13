cbec467a55c4e323c33fa2833f070728e1fb9c3a
andela-wakinsanmi <waleola.akinsanmi@andela.com>
2017-04-13 20:31:42 +0100

    Change ShouldShowAvatarInOnOnOneConversations default to be false

--- layer-atlas/src/main/java/com/layer/atlas/AtlasMessagesRecyclerView.java
+++ layer-atlas/src/main/java/com/layer/atlas/AtlasMessagesRecyclerView.java
@@ -41,7 +41,7 @@ public class AtlasMessagesRecyclerView extends RecyclerView {
     private AtlasMessagesAdapter mAdapter;
     private LinearLayoutManager mLayoutManager;
     private ItemTouchHelper mSwipeItemTouchHelper;
-    private boolean mShouldShowAvatar;
+    private boolean mShouldShowAvatarsInOneOnOneConversations;
 
     private MessageStyle mMessageStyle;
 
@@ -87,7 +87,7 @@ public class AtlasMessagesRecyclerView extends RecyclerView {
             }
         });
 
-        setShouldShowAvatarInOneOnOneConversations(mShouldShowAvatar);
+        setShouldShowAvatarInOneOnOneConversations(mShouldShowAvatarsInOneOnOneConversations);
         return this;
     }
 
@@ -250,7 +250,7 @@ public class AtlasMessagesRecyclerView extends RecyclerView {
         messageStyleBuilder.myBubbleColor(ta.getColor(R.styleable.AtlasMessagesRecyclerView_myBubbleColor, context.getResources().getColor(R.color.atlas_color_primary_blue)));
         messageStyleBuilder.otherBubbleColor(ta.getColor(R.styleable.AtlasMessagesRecyclerView_theirBubbleColor, context.getResources().getColor(R.color.atlas_color_primary_gray)));
 
-        mShouldShowAvatar = ta.getBoolean(R.styleable.AtlasMessagesRecyclerView_shouldShowAvatar, true);
+        mShouldShowAvatarsInOneOnOneConversations = ta.getBoolean(R.styleable.AtlasMessagesRecyclerView_setShouldShowAvatarsInOneOnOneConversations, false);
         ta.recycle();
         this.mMessageStyle = messageStyleBuilder.build();
     }
